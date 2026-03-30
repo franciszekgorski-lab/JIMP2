552e3c63652d4c73ae886527a2b5bd525950ab70
arkad <arkadiusz.wolk@outlook.com>
2026-03-31 01:43:09 +0200

    Wywoływanie algorytmów w main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 #include "graph.h"
 #include "Tutte.h"
@@ -10,10 +11,11 @@ int main(int argc, char* argv[]) {
 	int opt;
         char* input_file;
         char* output_file;
-	char flags = 0b0000;
+		char flags = 0b00000000;
+	
         Graph g = Graph_Construct();
 
-	while ( (opt = getopt(argc, argv, "f:i:o:p")) != -1 ) {
+	while ( (opt = getopt(argc, argv, "f:i:o:pa:")) != -1 ) {
 		switch (opt) {
 
 		case 'i':
@@ -37,6 +39,14 @@ int main(int argc, char* argv[]) {
 			}
 			break;
 
+		case 'a':
+			if (strcmp(optarg, "fruchterman") == 0) {
+				flags |= 0b00010000;
+			} else if (strcmp(optarg, "tutte") != 0) {
+				printf("The program defaults to the Fruchterman-Reingold algorithm. To use the Tutte algorithm, specify the 'tutte' argument preceded by the -a flag.");
+			}
+			break;
+
 		case '?':
 	               	//invalid opt
 			printf("[ERROR] ; Unknown option\n");
@@ -45,17 +55,23 @@ int main(int argc, char* argv[]) {
 	}
 
 	if ( (flags&0b0001) == 0b0001) {
-                Wczytywanie(g, input_file);
-                ForceFieldSim(g, 1, 100000);
+        Wczytywanie(g, input_file);
 	}
-	
+
+	if ( (flags&0b00010000) == 0b00010000) {
+		fruchterman_reingold(g, 500, time(NULL));
+	} else {
+		ForceFieldSim(g, 1, 100000);
+	}
+
 	if ( (flags&0b0010) == 0b0010) {
-                Zapisywanie(g, output_file, flags);
+        Zapisywanie(g, output_file, flags);
 	}
 
 	if ( (flags&0b0100) == 0b0100) {
 		Graph_Print(g);
 	}
+
 
 	return 0;
 }
